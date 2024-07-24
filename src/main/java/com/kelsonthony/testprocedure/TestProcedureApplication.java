e1396cf1eac7cedd998617d70a5c0557758f6b75
kelsonthony <k.thony@gmail.com>
2024-07-24 15:09:29 -0300

    Update

---
+++ src/main/java/com/kelsonthony/testprocedure/TestProcedureApplication.java
@@ -0,0 +1,13 @@
+package com.kelsonthony.testprocedure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProcedureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProcedureApplication.class, args);
+	}
+
+}
